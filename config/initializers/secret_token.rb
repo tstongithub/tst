c062f19969ed2e93cbcf6e67bc8d84fa8820d94f
tst <John.Smith.ne@gmail.com>
2012-09-26 09:55:37 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tst::Application.config.secret_token = '84def362e1434c3bc9768f78ea214faef7db08c545b0152c0e128de84208f52bfe0af32d653d4b4c3982a7067a40ccf7cc47f7c045b80ce3967b1d9575add242'
